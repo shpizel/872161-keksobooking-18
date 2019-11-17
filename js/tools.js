6faa42a30e13fed0dfe77e8b84d91013060bb8ce
Igor Shpizel <shpizel@yandex-team.ru>
2019-11-17 16:59:29 +0300

    повторно убираю лишний код

--- js/tools.js
+++ js/tools.js
@@ -17,32 +17,6 @@
     };
   };
 
-  var getRandomNumber = function (min, max) {
-    return min + Math.floor(Math.random() * (max - min + 1));
-  };
-
-  var shuffle = function (a) {
-    for (var i = a.length - 1; i > 0; i--) {
-      var j = Math.floor(Math.random() * i);
-      var temp = a[i];
-      a[i] = a[j];
-      a[j] = temp;
-    }
-    return a;
-  };
-
-  var getRandomChoice = function (array, length) {
-    return (length) ? shuffle(array).slice(0, length) : array[getRandomNumber(0, array.length - 1)];
-  };
-
-  var addLeadingZero = function (number, size) {
-    var s = String(number);
-    while (s.length < (size || 2)) {
-      s = '0' + s;
-    }
-    return s;
-  };
-
   var pluralize = function (count, one, two, three) {
     if (!Number.isInteger(count)) {
       return two;
@@ -145,11 +119,7 @@
   };
 
   window.tools = {
-    shuffle: shuffle,
-    getRandomNumber: getRandomNumber,
-    getRandomChoice: getRandomChoice,
     getCoords: getCoords,
-    addLeadingZero: addLeadingZero,
     pluralize: pluralize,
     disableNodes: disableNodes,
     enableNodes: enableNodes,
